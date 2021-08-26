b67e79d2aa9c44db4fccc40e54a4e6d6d21a57fe
Cyril Govin <cyril.govin@campus-igs-toulouse.fr>
2021-08-26 10:11:45 +0200

    destination address_prefix added

--- main.tf
+++ main.tf
@@ -51,14 +51,15 @@ resource "azurerm_network_security_group" "rg-govin-nsg" {
   resource_group_name = azurerm_resource_group.rg-govin.name
 
   security_rule {
-    name                   = "SSH"
-    priority               = 1001
-    direction              = "Inbound"
-    access                 = "Allow"
-    protocol               = "Tcp"
-    source_port_range      = "*"
-    destination_port_range = "22"
-    source_address_prefix  = "*"
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
   }
 
   tags = {
